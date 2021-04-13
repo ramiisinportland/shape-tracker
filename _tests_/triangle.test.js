885e67d5ba8016f6bd6f755056abec30aae29442
Rami <ramiahmedportland@gmail.com>
2021-04-13 13:26:59 -0700

    Add more tisting

---
+++ _tests_/triangle.test.js
@@ -0,0 +1,12 @@
+// import { TestScheduler } from "jest-cli";
+import Triangle from "./../src/triangle.js";
+
+describe('Triangle', () => {
+  test('should correctly create a triangle object with three lengthn make and equilateral trangle' , () => {
+    const equiTriangle  = new Triangle (5,5,5);
+    expect(scalTriangle.checkType()).toEqual("scalene a triangle")
+    // expect(triangle.side1).toEqual(2);
+    // expect(triangle.side2).toEqual(4);
+    // expect(triangle.side3).toEqual(5);
+  });
+});
