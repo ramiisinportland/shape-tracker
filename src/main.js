741c1cd5231be64e8358d2f2bd27137f1acd1cb3
Rami <ramiahmedportland@gmail.com>
2021-04-12 21:17:48 -0700

    Add eslintrc

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import $ from 'jquery';
+import Triangle from './triangle.js';
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
