0c0d5db50b80a787c3fe082d08bf608d4f91fca6
Rami <ramiahmedportland@gmail.com>
2021-04-13 14:33:49 -0700

    add more tisting

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -4,7 +4,7 @@ import Triangle from "./../src/triangle.js";
 describe('Triangle', () => {
   test('should correctly create a triangle object with three lengthn make and equilateral trangle' , () => {
     const equiTriangle  = new Triangle (5,5,5);
-    expect(scalTriangle.checkType()).toEqual("scalene a triangle")
+   expect(equiTriangle.checkType()).toEqual("equilateral triangle");
     // expect(triangle.side1).toEqual(2);
     // expect(triangle.side2).toEqual(4);
     // expect(triangle.side3).toEqual(5);
--- src/triangle.js
+++ src/triangle.js
@@ -1,11 +1,7 @@
 export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
-  this.side3 = side3;
-}
-
-Triangle.prototype.checkType = function() {
-  return "I can't answer that yet!";
+  this.side3 = side2;
 }
 
 Triangle.prototype.checkType = function() {
@@ -13,8 +9,8 @@ Triangle.prototype.checkType = function() {
     return "not a triangle";
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
     return "scalene triangle";
-  } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
-      return "equilateral triangle";
+  }  else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+    return "equilateral triangle";
   } else {
     return "isosceles triangle";
   }
